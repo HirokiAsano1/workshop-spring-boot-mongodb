792cefb718b0fd152f8b2e7a99b14d7efb341188
hirokiasano1 <hirokiasanovieira@gmail.com>
2024-07-31 23:20:57 -0300

    Mongod connection

--- src/main/java/com/example/workshopmongo/Entities/User.java
+++ src/main/java/com/example/workshopmongo/Entities/User.java
@@ -1,6 +1,7 @@
 package com.example.workshopmongo.Entities;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -10,6 +11,8 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+
+@Document(collation = "User") //substitui o @Entity do mysQL
 public class User implements Serializable {
 
     @Serial
