6416f286a83759f849f12f0bbdcf6fccc9745db6
hirokiasano1 <hirokiasanovieira@gmail.com>
2024-08-08 01:02:31 -0300

    Crud Operations

--- src/main/java/com/example/workshopmongo/Controller/UserController.java
+++ src/main/java/com/example/workshopmongo/Controller/UserController.java
@@ -5,11 +5,10 @@ import com.example.workshopmongo.Entities.User;
 import com.example.workshopmongo.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -38,4 +37,33 @@ public class UserController {
         return ResponseEntity.ok().body(new UserDTO(obj)); // converter para userDTO
 
     }
+
+    @PostMapping
+    public ResponseEntity<Void> Save (@RequestBody UserDTO objDto)
+    {
+        User obj = userService.fromDto(objDto);
+        obj = userService.save(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // linha responsavel por retorna um cabeçario contendo o caminho do obj criado
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update (@RequestBody UserDTO objDto,@PathVariable String id)
+    {
+        User obj = userService.fromDto(objDto);
+        obj.setId(id);
+        obj = userService.update(obj);
+        return ResponseEntity.noContent().build(); //cod204
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity <Void> Delete(@PathVariable String id)
+    {
+     userService.delete(id);
+
+     return ResponseEntity.noContent().build(); //cod204
+
+    }
+
+
 }
--- src/main/java/com/example/workshopmongo/Services/UserService.java
+++ src/main/java/com/example/workshopmongo/Services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.Services;
 
+import com.example.workshopmongo.DTO.UserDTO;
 import com.example.workshopmongo.Entities.User;
 import com.example.workshopmongo.Excpetions.ObjectNotFoundException;
 import com.example.workshopmongo.Repository.UserRepository;
@@ -22,5 +23,33 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update (User obj)
+    {
+        User newobj = userRepository.findById(obj.getId()).get();
+        updateData(newobj,obj);
+        return userRepository.save(newobj);
+    }
+
+    private void updateData(User newobj, User obj) {
+        newobj.setName(obj.getName());
+        newobj.setEmail(obj.getEmail());
+    }
+
+    public void delete (String id)
+    {
+        findById(id); //reutilizei o metodo de exceçao acima para o lançar execeçao no medodo delete tbm
+        userRepository.deleteById(id);
+    }
+
+
+    public User fromDto(UserDTO obj)
+    {
+        return new User(obj.getId(), obj.getName(), obj.getEmail());
     }
 }
